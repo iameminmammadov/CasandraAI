a5a503e8b098323d91ed457f471d1a9ac7953716
Emin Mammadov <emammadov@acerta.ca>
2020-05-24 00:01:39 -0400

    minor cleanup

--- recommender_system/dash_images.py
+++ recommender_system/dash_images.py
@@ -18,9 +18,6 @@ from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 
 
-
-
-#external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 server = app.server
 
